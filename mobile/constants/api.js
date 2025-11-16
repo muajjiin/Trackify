2bb9244903ab7bb5d6ec6b39b471ef26012f7448
elisha <korosenseimark40@gmail.com>
2025-11-16 10:09:58 +0600

    add:create page ui & logic

---
+++ mobile/constants/api.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://trackify-dj90.onrender.com/api";
+// const API_URL = "https://wallet-api-cxqp.onrender.com/api";
+// export const API_URL = "http://localhost:5001/api";
